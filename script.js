3158f4cd5b5779e097c0125a41111f6216f22c8b
Edward B <ebotelloaroca00@gmail.com>
2023-09-01 19:05:54 -0500

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,27 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+const form = document.querySelector('#form');
+
+
+// form.addEventListener('submit', sumarInputValues)
+
+// function sumarInputValues(event) {
+//     console.log({event});
+//     event.preventDefault();
+
+//     const sumaInputs = Number(input1.value) + Number(input2.value);
+//     pResult.innerText = "Resultado: " + sumaInputs;
+// }
+
+
+btn.addEventListener('click', sumarInputValues)
+
+function sumarInputValues() {
+    const sumaInputs = Number(input1.value) + Number(input2.value);
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+
+// EVENTOS https://developer.mozilla.org/es/docs/Web/Events
